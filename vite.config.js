767ccf75bcc006aafb9a04d192a097f9ad990fa4
akirayorunoe <akirayorunoe@gmail.com>
2025-03-01 19:39:28 +0700

    Update: github setup

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
         entryFileNames: 'assets/[name].js',
         chunkFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name].[ext]',
+        manualChunks: undefined, // Ngăn Rollup tạo file `__virtual__`
       },
     },
   },
