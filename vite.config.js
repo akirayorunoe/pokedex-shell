77261a68f9ad1eb5fb23f79e8daad7b163e115df
akirayorunoe <akirayorunoe@gmail.com>
2025-03-01 18:58:07 +0700

    Update: github setup

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,12 @@ export default defineConfig({
   ],
   build: {
     target: "esnext",
-    minify: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
   },
 })
